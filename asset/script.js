5405e399529753df80b725474da522372dd3686f
Ummul Mukta <ummulcolumbia@gmail.com>
2024-03-12 20:35:58 -0400

    added code in js

--- asset/script.js
+++ asset/script.js
@@ -54,6 +54,31 @@ function fetchPrice(crypto) {
         $('#priceDisplay').text('Price: Unable to fetch price');
     });
 }
+// Get the modal
+var modal = document.getElementById("customModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on the button, open the modal 
+function showModal(message) {
+  document.getElementById("modalText").textContent = message;
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 
 
 
